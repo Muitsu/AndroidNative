d0705ba43452a3c1a4dc0d30273b0b317a9a04c4
Muitsu <blankdahma@gmail.com>
2024-06-14 17:48:20 +0800

    remove unwanted code

--- app/src/main/java/com/example/messageapp/api_service/ApiClient.java
+++ app/src/main/java/com/example/messageapp/api_service/ApiClient.java
@@ -1,13 +1,5 @@
 package com.example.messageapp.api_service;
 
-import androidx.annotation.NonNull;
-
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
@@ -26,26 +18,4 @@ public class ApiClient {
         return getClient(null);
     }
 
-    // Generic method to handle Retrofit calls
-    public static <T> T executeCall(Call<T> call) throws ExecutionException, InterruptedException {
-        CompletableFuture<T> future = new CompletableFuture<>();
-
-        call.enqueue(new Callback<T>() {
-            @Override
-            public void onResponse(@NonNull Call<T> call, @NonNull Response<T> response) {
-                if (response.isSuccessful()) {
-                    future.complete(response.body());
-                } else {
-                    future.completeExceptionally(new Throwable("Response not successful: " + response.code()));
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<T> call, @NonNull Throwable throwable) {
-                future.completeExceptionally(throwable);
-            }
-        });
-
-        return future.get();
-    }
 }
